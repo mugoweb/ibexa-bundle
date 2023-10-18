10403a566d5057c1617eeb08a65ca7172a78f523
pkamps <pkamps@mugo.ca>
2023-10-18 13:27:40 +0200

    Clearing broken aliases (missing parent element)

--- src/Controller/ToolsController.php
+++ src/Controller/ToolsController.php
@@ -3,9 +3,11 @@
 namespace MugoWeb\IbexaBundle\Controller;
 
 use eZ\Publish\API\Repository\Repository;
+use eZ\Publish\Core\Base\Exceptions\UnauthorizedException;
+use eZ\Publish\Core\Base\Exceptions\NotFoundException;
+use eZ\Publish\Core\Base\Exceptions\BadStateException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Ibexa\Core\MVC\Symfony\Security\Authorization\Attribute;
 
 class ToolsController extends AbstractController
 {
@@ -21,7 +23,7 @@ class ToolsController extends AbstractController
         {
             try {
                 $msg = $repository->sudo(
-                    function (Repository $repository) use ($location)
+                    function (Repository $repository) use ( $locationId )
                     {
                         try
                         {
@@ -36,10 +38,37 @@ class ToolsController extends AbstractController
                             $location
                         );
 
+                        // Recursive function to delete broken (missing parent) path elements
+                        // The way deleteCorruptedUrlAliases works forces us to run it multiple times
+                        // until we delete all corupted URL Aliases
+                        function listCustomURLAliases( $repository, $location, $iteration )
+                        {
+                            try
+                            {
+                                $urlAliases = $repository->getURLAliasService()->listLocationAliases( $location );
+                            }
+                            catch( BadStateException $e)
+                            {
+                                // Avoid endless recursion - a DB master/slave setup has maybe issues with this
+                                if( $iteration < 20 )
+                                {
+                                    // It tries to fix all corrupted URL Aliases (not only for the location context)
+                                    // Not very efficient but it takes only a few seconds to run that function.
+                                    $repository->getURLAliasService()->deleteCorruptedUrlAliases();
+
+                                    listCustomURLAliases( $repository, $location, $iteration + 1 );
+                                }
+                            }
+                        }
+
+                        listCustomURLAliases( $repository, $location, 0 );
+
                         return 'Url Aliases regenerated.';
                     }
                 );
-            } catch (Exception $e) {
+            }
+            catch (Exception $e)
+            {
                 $contentInfo = $location->getContentInfo();
                 $msg = sprintf(
                     'Failed processing location %d - [%d] %s (%s: %s)',
